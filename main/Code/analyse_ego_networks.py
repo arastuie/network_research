a57819c079fca383ef4fbd6865f9bbea90be0a32
Makan Arastuie <makan.arastuie@gmail.com>
2017-06-28 00:23:48 -0400

    Add gplus hop degree analysis

--- main/Code/analyse_ego_networks.py
+++ main/Code/analyse_ego_networks.py
@@ -2,12 +2,15 @@ import pickle
 import numpy as np
 import helpers as h
 import networkx as nx
+from joblib import Parallel, delayed
+import os
 import degree_based_analysis as a1
 import num_cluster_per_cn_analysis as a2
 import tot_num_cluster_of_all_cn_analysis as a3
 import tot_num_cluster_based_num_cn_analysis as a4
 import percent_of_cn_belonging_to_same_cluster as a5
 import hop_degree_analysis as a6
+import gplus_hop_degree_analysis as a6
 
 
 # # reading FB graph
@@ -17,39 +20,32 @@ import hop_degree_analysis as a6
 # ego_centric_networks, ego_nodes = h.get_ego_centric_networks_in_fb(original_graph, 50, "random_50_ego_nets.pckl",
 #                                                                    search_type='random', hop=2, center=True)
 
-print("Loading Facebook random 200 ego centric networks...")
-
-with open('../Data/random_200_ego_nets.pckl', 'rb') as f:
-    ego_centric_networks, ego_nodes = pickle.load(f)
+# print("Loading Facebook random 200 ego centric networks...")
+#
+# with open('../Data/random_200_ego_nets.pckl', 'rb') as f:
+#     ego_centric_networks, ego_nodes = pickle.load(f)
 
 print("Analysing ego centric networks...")
 
-num_cluster = 8
-# f = []
-# nf = []
+# num_cluster = 8
 
-for o in range(len(ego_centric_networks)):
-    print("EgoNet #%d" % o)
+# for o in range(len(ego_centric_networks)):
+#     print("EgoNet #%d" % o)
+#
+#     a1.run_degree_based_analysis(ego_centric_networks[o], ego_nodes[o], o, False, '../Plots/degree_based')
+#
+#     a2.run_num_cluster_per_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                        '../Plots/cluster_per_node')
+#
+#     a3.run_tot_num_cluster_of_all_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                               '../Plots/total_cluster/6-2-17-13-30')
+#
+#     a4.run_tot_num_cluster_based_num_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                                  '../Plots/total_cluster_overall')
+#
+#     a5.run_percent_of_cn_belonging_to_same_cluster(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                                    '../Plots/percent_of_cn_belonging_to_same_cluster')
+#
+#     a6.run_hop_degree_analysis(ego_centric_networks[o], ego_nodes[o], o, True, '../Plots/gplus_hop_degree_based')
 
-    # a1.run_degree_based_analysis(ego_centric_networks[o], ego_nodes[o], o, False, '../Plots/degree_based')
-
-    # a2.run_num_cluster_per_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
-    #                                    '../Plots/cluster_per_node')
-
-    # a3.run_tot_num_cluster_of_all_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
-    #                                           '../Plots/total_cluster/6-2-17-13-30')
-
-    # if formed_mean != -1:
-    #     f.append(formed_mean)
-    #     nf.append(not_formed_mean)
-    #
-    # print(np.mean(f))
-    # print(np.mean(nf))
-
-    # a4.run_tot_num_cluster_based_num_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
-    #                                              '../Plots/total_cluster_overall')
-
-    # a5.run_percent_of_cn_belonging_to_same_cluster(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
-    #                                                '../Plots/percent_of_cn_belonging_to_same_cluster')
-
-    a6.run_hop_degree_analysis(ego_centric_networks[o], ego_nodes[o], o, True, '../Plots/hop_degree_based')
+Parallel(n_jobs=10)(delayed(a6.gplus_run_hop_degree_analysis)(ego_node_file, True, '../Plots/gplus_hop_degree_based') for ego_node_file in os.listdir('../Data/gplus-ego'))
---
+++ main/Code/gplus_hop_degree_analysis.py
@@ -0,0 +1,102 @@
+import sys
+import helpers as h
+import networkx as nx
+import numpy as np
+import pickle
+
+
+def gplus_run_hop_degree_analysis(ego_net_file, save_plot=False, plot_save_path=''):
+
+    """
+    Degree based analysis (with the second hop):
+        1. Finds all the nodes in the second hop which formed and did not form an edge with the ego node in the next
+           snapshot
+        2. Finds the degree of all those nodes in a given hop
+        3. Plots a histogram comparing the degree of nodes in the second hop which formed an edge with the ego node vs
+           the node that did not.
+
+    :param ego_net_snapshots: Snapshots of an ego-centric network
+    :param ego_node: The ego node
+    :param ego_net_num: The number of the ego network, only used for the plot title
+    :param save_plot: If true, saves the plot, also a path should be passed as the next argument.
+    :param plot_save_path: Path to save the plot. ex: '../Plots/degree_based'
+    """
+
+    # Exit if plot should be saved, put there is no path
+    if save_plot and plot_save_path == '':
+        print(sys.stderr, "Please provide the path to which plots should be saved.")
+        sys.exit(1)
+
+    with open('../Data/gplus-ego/%s' % ego_net_file, 'rb') as f:
+        ego_node, ego_net = pickle.load(f)
+
+    # return if the network has less than 30 nodes
+    if nx.number_of_nodes(ego_net) < 100:
+        return
+
+    ego_net_snapshots = []
+
+    for r in range(4):
+        ego_net_snapshots.append(nx.Graph([(u, v, d) for u, v, d in ego_net.edges(data=True)
+                                 if d['snapshot'] <= r]))
+
+    degree_formed_in_snapshots = []
+    degree_not_formed_in_snapshots = []
+
+    # only goes up to one to last snap, since it compares every snap with the next one, to find formed edges.
+    for i in range(len(ego_net_snapshots) - 1):
+        formed_edges_nodes_with_second_hop, not_formed_edges_nodes_with_second_hop, current_snap_first_hop_nodes = \
+            h.get_nodes_of_formed_and_non_formed_edges_between_ego_and_second_hop(ego_net_snapshots[i],
+                                                                                  ego_net_snapshots[i + 1],
+                                                                                  ego_node, True)
+
+        # <editor-fold desc="Analyze formed edges">
+        # List of degrees of nodes in the second hop which formed an edge with the ego node
+        degree_formed = []
+        for u in formed_edges_nodes_with_second_hop:
+            common_neighbors = nx.common_neighbors(ego_net_snapshots[i], u, ego_node)
+            temp_degree_formed = []
+
+            for c in common_neighbors:
+                # first hop test
+                # temp_degree_formed.append(len([n for n in ego_net_snapshots[i].neighbors(c) if n in
+                #                           current_snap_first_hop_nodes]))
+
+                # second hop test
+                temp_degree_formed.append(len([n for n in ego_net_snapshots[i].neighbors(c) if n not in
+                                          current_snap_first_hop_nodes]) - 1)
+
+            degree_formed.append(np.mean(temp_degree_formed))
+        # </editor-fold>
+        # <editor-fold desc="Analyze not formed edges">
+        # List of degrees of nodes in the second hop which did not form an edge with the ego node
+        degree_not_formed = []
+        for u in not_formed_edges_nodes_with_second_hop:
+            common_neighbors = nx.common_neighbors(ego_net_snapshots[i], u, ego_node)
+            temp_degree_not_formed = []
+
+            for c in common_neighbors:
+                # first hop test
+                # temp_degree_not_formed.append(len([n for n in ego_net_snapshots[i].neighbors(c) if n in
+                #                               current_snap_first_hop_nodes]))
+
+                # second hop test
+                temp_degree_not_formed.append(len([n for n in ego_net_snapshots[i].neighbors(c) if n not in
+                                              current_snap_first_hop_nodes]) - 1)
+
+            degree_not_formed.append(np.mean(temp_degree_not_formed))
+        # </editor-fold>
+
+        if len(degree_formed) != 0 and len(degree_not_formed) != 0:
+            degree_formed_in_snapshots.append(degree_formed)
+            degree_not_formed_in_snapshots.append(degree_not_formed)
+
+    if len(degree_formed_in_snapshots) != 0:
+        if save_plot:
+            plot_save_path += '/%d_hop_degree.png' % ego_node
+
+        h.plot_formed_vs_not('degree', degree_formed_in_snapshots,
+                             degree_not_formed_in_snapshots, plot_number=ego_node, save_plot=save_plot,
+                             save_path=plot_save_path)
+
+    print("Graph analyzed!")
