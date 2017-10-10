4ed57d0b96d4b52a816af03a55f025f872d09c21
Makan Arastuie <makan.arastuie@gmail.com>
2017-10-09 21:14:03 -0400

    Before clean up

---
+++ main/Code/analyse_ego_networks_plotter.py
@@ -0,0 +1,175 @@
+import pickle
+import numpy as np
+import helpers as h
+import networkx as nx
+from joblib import Parallel, delayed
+import os
+import degree_based_analysis as a1
+import num_cluster_per_cn_analysis as a2
+import tot_num_cluster_of_all_cn_analysis as a3
+import tot_num_cluster_based_num_cn_analysis as a4
+import percent_of_cn_belonging_to_same_cluster as a5
+import hop_degree_analysis_temp as a6
+#import gplus_hop_degree_directed_analysis_t06 as a7
+import gplus_hop_degree_directed_analysis as a7
+import adamic_adar_analysis as a8
+import matplotlib.pyplot as plt
+
+
+# # reading FB graph
+# original_graph = h.read_facebook_graph()
+#
+# # extracting ego centric networks
+# ego_centric_networks, ego_nodes = h.get_ego_centric_networks_in_fb(original_graph, 50, "random_50_ego_nets.pckl",
+#                                                                    search_type='random', hop=2, center=True)
+#
+
+# print("Loading Facebook random 200 ego centric networks...")
+
+print("Analysing ego centric networks...")
+path = '../Results/fb-empirical-global-local-results/local/lower-6/temp'
+
+
+def run_analysis(index):
+    mfems = []
+    mnfems = []
+
+    cnt = 0
+
+    for ego_net_file in os.listdir('../Data/fb-egonets/{0}'.format(index)):
+        with open('../Data/fb-egonets/{0}/{1}'.format(index, ego_net_file), 'rb') as f:
+            egonet_snapshots, ego_node = pickle.load(f)
+
+        mfem, mnfem = a6.run_hop_local_degree_analysis(egonet_snapshots, ego_node, range(0, 5))
+        # mfem, mnfem = a8.run_hop_global_degree_analysis(egonet_snapshots, ego_node, 0, False, '../Plots/global_degree')
+
+        if mfem != -1:
+            mfems.append(mfem)
+            mnfems.append(mnfem)
+
+        cnt = cnt + 1
+
+        if cnt % 500 == 0:
+            print("Index {0} -> {1}".format(index, cnt))
+
+    if len(mfems) > 0:
+        print("mfem -> {0}".format(np.mean(mfems)))
+        print("mnfem -> {0}".format(np.mean(mnfems)))
+
+        with open('{0}/{1}-egonodes-result.pckl'.format(path, index), 'wb') as f:
+            pickle.dump([mfems, mnfems], f, protocol=-1)
+    else:
+        print("No analysis in index {0}".format(index))
+
+
+Parallel(n_jobs=28)(delayed(run_analysis)(i) for i in range(0, 28))
+
+print("Merging all files...")
+
+mfems = []
+mnfems = []
+for result_file in os.listdir(path):
+    with open('{0}/{1}'.format(path, result_file), 'rb') as f:
+        mf, mn = pickle.load(f)
+
+    mfems = mfems + mf
+    mnfems = mnfems + mn
+
+with open('{0}/total-result.pckl'.format(path), 'wb') as f:
+    pickle.dump([mfems, mnfems], f, protocol=-1)
+
+# with open('{0}/total-result.pckl'.format(path), 'rb') as f:
+#     mfems, mnfems = pickle.load(f)
+
+plot_path = '../Results/fb-empirical-global-local-results/local/lower-6'
+
+plt.hist(mfems, color='r', alpha=0.8, weights=np.zeros_like(mfems) + 1. / len(mfems),
+         label="MFEM: {0:.2f}".format(np.mean(mfems)))
+
+plt.hist(mnfems, color='b', alpha=0.5, weights=np.zeros_like(mnfems) + 1. / len(mnfems),
+         label="MNFEM: {0:.2f}".format(np.mean(mnfems)))
+
+plt.legend(loc='upper right')
+plt.ylabel('Relative Frequency')
+plt.xlabel('Mean Normalized Local Degree of Common Neighbors')
+
+current_fig = plt.gcf()
+current_fig.savefig('{0}/overall-mean-normal.eps'.format(plot_path), format='eps', dpi=1000)
+current_fig.savefig('{0}/overall-mean-normal-png.png'.format(plot_path))
+
+print("Number of egonets analyzed: {0}".format(len(mfems)))
+print("Done")
+
+
+# num_cluster = 8
+
+# for o in range(len(ego_centric_networks)):
+#     print("EgoNet #%d" % o)
+#
+#     a1.run_degree_based_analysis(ego_centric_networks[o], ego_nodes[o], o, False, '../Plots/degree_based')
+#
+#     a2.run_num_cluster_per_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                        '../Plots/cluster_per_node')
+#
+#     a3.run_tot_num_cluster_of_all_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                               '../Plots/total_cluster/6-2-17-13-30')
+#
+#     a4.run_tot_num_cluster_based_num_cn_analysis(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                                  '../Plots/total_cluster_overall')
+#
+#     a5.run_percent_of_cn_belonging_to_same_cluster(ego_centric_networks[o], ego_nodes[o], num_cluster, o, True,
+#                                                    '../Plots/percent_of_cn_belonging_to_same_cluster')
+#
+#     a6.run_hop_degree_analysis(ego_centric_networks[o], ego_nodes[o], o, True, '../Plots/gplus_hop_degree_based')
+#
+# Parallel(n_jobs=20)(delayed(a6.gplus_run_hop_degree_analysis)(ego_node_file, True, '../Plots/gplus_hop_degree_based')
+#                     for ego_node_file in os.listdir('../Data/gplus-ego/first-hop-nodes'))
+
+# Parallel(n_jobs=20)(delayed(a6.run_hop_degree_analysis)
+#                     (ego_centric_networks[o], ego_nodes[o], o, True, '../Plots/hop_degree_based')
+#                     for o in range(len(ego_centric_networks)))
+
+# Parallel(n_jobs=20)(delayed(a7.gplus_run_hop_degree_directed_analysis)
+#                     (ego_node_file, True, '../Plots/gplus_hop_degree_based')
+#                     for ego_node_file in os.listdir('../Data/gplus-ego/first-hop-nodes'))
+#
+# triangle_types = ['T01', 'T02', 'T03', 'T04', 'T05', 'T06', 'T07', 'T08', 'T09']
+#
+#
+# def test_directed_triangle(triangle_type):
+#     overall_means = {
+#         'formed_in_degree_first_hop': [],
+#         'not_formed_in_degree_first_hop': [],
+#         'formed_in_degree_second_hop': [],
+#         'not_formed_in_degree_second_hop': [],
+#         'formed_out_degree_first_hop': [],
+#         'not_formed_out_degree_first_hop': [],
+#         'formed_out_degree_second_hop': [],
+#         'not_formed_out_degree_second_hop': [],
+#     }
+#
+#     for ego_net_file in os.listdir('../Data/gplus-ego/first-hop-nodes'):
+#         a7.gplus_run_hop_degree_directed_analysis('../Data/gplus-ego/first-hop-nodes/%s' % ego_net_file, triangle_type,
+#                                                   overall_means, True, '../Plots/hop_degree_based')
+#
+#     with open('../Plots/hop_degree_based/{0}/overall.txt'.format(triangle_type), 'w') as info_file:
+#         info_file.write("OVERALL SCORES:\n")
+#         info_file.write("In-degree First Hop:\n\tFEM:{0:.3f} \t NFEM:{1:.3f}\n\n"
+#                         .format(np.mean(overall_means['formed_in_degree_first_hop']),
+#                                 np.mean(overall_means['not_formed_in_degree_first_hop'])))
+#
+#         info_file.write("In-degree Second Hop:\n\tFEM:{0:.3f} \t NFEM:{1:.3f}\n\n"
+#                         .format(np.mean(overall_means['formed_in_degree_second_hop']),
+#                                 np.mean(overall_means['not_formed_in_degree_second_hop'])))
+#
+#         info_file.write("Out-degree First Hop:\n\tFEM:{0:.3f} \t NFEM:{1:.3f}\n\n"
+#                         .format(np.mean(overall_means['formed_out_degree_first_hop']),
+#                                 np.mean(overall_means['not_formed_out_degree_first_hop'])))
+#
+#         info_file.write("Out-degree Second Hop:\n\tFEM:{0:.3f} \t NFEM:{1:.3f}\n\n"
+#                         .format(np.mean(overall_means['formed_out_degree_second_hop']),
+#                                 np.mean(overall_means['not_formed_out_degree_second_hop'])))
+#
+#     print("Done with {0} triangle type".format(triangle_type))
+#
+# Parallel(n_jobs=9)(delayed(test_directed_triangle)(tri_type) for tri_type in triangle_types)
---
+++ main/Code/link_prediction_analysis_plotter.py
@@ -0,0 +1,233 @@
+import pickle
+import helpers
+import link_prediction_helpers as lp_helpers
+import linear_regression_link_prediction as lr
+import numpy as np
+import os
+import networkx as nx
+from joblib import Parallel, delayed
+
+# print("Reading in 200 random Facebook ego networks...")
+#
+# with open('../Data/random_200_ego_nets.pckl', 'rb') as f:
+#     ego_centric_networks, ego_nodes = pickle.load(f)
+
+# with open('../Data/first_50_ego.pckl', 'rb') as f:
+#     ego_centric_networks, ego_nodes = pickle.load(f)
+
+# print("Networks in!")
+
+# lp_results = []
+# # for w in np.arange(10, 20, 2):
+# for i in range(len(ego_centric_networks)):
+#     scores = lp_helpers.run_adamic_adar_on_ego_net(ego_centric_networks[i], ego_nodes[i])
+#
+#     if scores is not None:
+#         lp_results.append(scores)
+#
+# lp_helpers.plot_auroc_hist(lp_results)
+# lp_helpers.plot_pr_hist(lp_results)
+
+# fb_graph = helpers.read_facebook_graph()
+#
+# orig_snapshots = []
+#
+# oldest_timestamp = 1157454929
+# seconds_in_90_days = 7776000
+# for i in range(10):
+#     orig_snapshots.append(nx.Graph([(u, v, d) for u, v, d in fb_graph.edges(data=True)
+#                                     if d['timestamp'] < (oldest_timestamp + seconds_in_90_days * i)]))
+# orig_snapshots.append(fb_graph)
+# fb_graph = None
+
+path = '/home/marastu2/ego_network_research/main/Results/fb-lp-results/lower-6/temp'
+top_k_values = [1, 3, 5, 10, 15, 20, 25, 30]
+
+def run_link_prediction(index):
+    percent_aa = {}
+    percent_dcaa = {}
+    percent_cn = {}
+    percent_dccn = {}
+
+    for k in top_k_values:
+        percent_aa[k] = []
+        percent_dcaa[k] = []
+        percent_cn[k] = []
+        percent_dccn[k] = []
+
+    for ego_net_file in os.listdir('../Data/fb-egonets/{0}'.format(index)):
+        with open('../Data/fb-egonets/{0}/{1}'.format(index, ego_net_file), 'rb') as f:
+            egonet_snapshots, ego_node = pickle.load(f)
+
+        aa, dcaa, cn, dccn = lp_helpers.run_adamic_adar_on_ego_net_ranking(egonet_snapshots, ego_node, top_k_values, range(0, 5))
+
+        for k in top_k_values:
+            percent_aa[k] = percent_aa[k] + aa[k]
+            percent_dcaa[k] = percent_dcaa[k] + dcaa[k]
+            percent_cn[k] = percent_cn[k] + cn[k]
+            percent_dccn[k] = percent_dccn[k] + dccn[k]
+
+    if len(percent_aa[top_k_values[0]]) > 0:
+        with open('{0}/{1}-lp-result.pckl'.format(path, index), 'wb') as f:
+            pickle.dump([percent_aa, percent_dcaa, percent_cn, percent_dccn], f, protocol=-1)
+    else:
+        print("No analysis in index {0}".format(index))
+
+
+Parallel(n_jobs=28)(delayed(run_link_prediction)(i) for i in range(0, 28))
+
+print("Merging all files...")
+
+percent_aa = {}
+percent_dcaa = {}
+percent_cn = {}
+percent_dccn = {}
+
+for k in top_k_values:
+    percent_aa[k] = []
+    percent_dcaa[k] = []
+    percent_cn[k] = []
+    percent_dccn[k] = []
+
+for result_file in os.listdir(path):
+    with open('{0}/{1}'.format(path, result_file), 'rb') as f:
+        aa, dcaa, cn, dccn = pickle.load(f)
+
+    for k in top_k_values:
+        percent_aa[k] = percent_aa[k] + aa[k]
+        percent_dcaa[k] = percent_dcaa[k] + dcaa[k]
+        percent_cn[k] = percent_cn[k] + cn[k]
+        percent_dccn[k] = percent_dccn[k] + dccn[k]
+
+with open('{0}/total-result.pckl'.format(path), 'wb') as f:
+    pickle.dump([percent_aa, percent_dcaa, percent_cn, percent_dccn], f, protocol=-1)
+
+for k in top_k_values:
+    print("For top {0}:".format(k))
+    aa = np.mean(percent_aa[k])
+    dcaa = np.mean(percent_dcaa[k])
+    print("\taa -> {0}".format(aa))
+    print("\tdcaa -> {0}\n".format(dcaa))
+    print("\tdcaa - aa -> {0}".format(dcaa - aa))
+
+    cn = np.mean(percent_cn[k])
+    dccn = np.mean(percent_dccn[k])
+    print("\tcn -> {0}".format(cn))
+    print("\tdccn -> {0}\n".format(dccn))
+    print("\tdccn - cn -> {0}".format(dccn - cn))
+
+
+
+
+# def run_directed_link_prediction_analysis(triangle_type):
+#     percent_scores = {
+#         'cn': {},
+#         'dccn_i': {},
+#         'dccn_o': {},
+#         'aa_i': {},
+#         'aa_o': {},
+#         'dcaa_i': {},
+#         'dcaa_o': {}
+#     }
+#
+#     top_k_values = [1, 3, 5, 10, 15, 20, 25, 30]
+#     for k in top_k_values:
+#         for ps in percent_scores.keys():
+#             percent_scores[ps][k] = []
+#     i = 0
+#     for ego_net_file in os.listdir('../Data/gplus-ego/first-hop-nodes'):
+#         percent_score_results = lp_helpers.run_link_prediction_comparison_on_directed_graph('../Data/gplus-ego/first-hop-nodes/%s' % ego_net_file, triangle_type, top_k_values)
+#
+#         if percent_score_results is None:
+#             continue
+#
+#         any_result = False
+#
+#         for ps in percent_scores.keys():
+#             for k in top_k_values:
+#                 for m in percent_score_results[ps][k]:
+#                     any_result = True
+#                     percent_scores[ps][k].append(m)
+#
+#         if any_result:
+#             i = i + 1
+#             print('{0} -> {1}'.format(triangle_type, i))
+#
+#     with open("../Results/directed_lp_result/{0}-directed_lp_result.txt".format(triangle_type), "w") as f:
+#         for k in top_k_values:
+#             f.write("\nFor top {0}:\n".format(k))
+#             for ps in percent_scores:
+#                 f.write("\t{0} -> {1}\n".format(ps, np.mean(percent_scores[ps][k])))
+
+
+# triangle_types = ['T01', 'T02', 'T03', 'T04', 'T05', 'T06', 'T07', 'T08', 'T09']
+# Parallel(n_jobs=9)(delayed(run_directed_link_prediction_analysis)(triangle_type) for triangle_type in triangle_types)
+
+# run_directed_link_prediction_analysis('T09')
+
+# degree_formation = None
+# for i in range(len(ego_centric_networks)):
+#     degrees = lp_helpers.ego_net_link_formation_hop_degree(ego_centric_networks[i], ego_nodes[i])
+#
+#     if degrees is not None:
+#         if degree_formation is None:
+#             degree_formation = np.array(degrees)
+#         else:
+#             degree_formation = np.concatenate((degree_formation, np.array(degrees)))
+#
+#     print('{0}'.format(i), end='\r')
+#
+# lp_helpers.plot_degree_scatter(degree_formation)
+
+# index_comparison = None
+# for i in range(len(ego_centric_networks)):
+#     comparison = lp_helpers.run_adamic_adar_on_ego_net_ranking_plot(ego_centric_networks[i], ego_nodes[i])
+#
+#     if comparison is not None:
+#         if index_comparison is None:
+#             index_comparison = np.array(comparison)
+#         else:
+#             index_comparison = np.concatenate((index_comparison, np.array(comparison)))
+#
+#     print('{0}'.format(i), end='\r')
+#
+# lp_helpers.plot_degree_scatter(index_comparison)
+
+# aa_only_scores = []
+# both_scores = []
+# cnt = 1
+# for path in os.listdir('../Data/fb_lp_features_reciprocal'):
+#     aa_only_scores.append(list(lr.run_linear_regression('../Data/fb_lp_features_reciprocal/{0}'.format(path), True)))
+#     both_scores.append(list(lr.run_linear_regression('../Data/fb_lp_features_reciprocal/{0}'.format(path), False)))
+#     print(cnt, end='\r')
+#     cnt += 1
+#
+# aa_only_scores = np.array(aa_only_scores)
+# print("AA Only:")
+# print("Mean AA Coefficient: {0}".format(np.mean(aa_only_scores[:, 0])))
+# print("Mean AUROC: {0}".format(np.mean(aa_only_scores[:, 1])))
+# print("Mean Avg Precision: {0}".format(np.mean(aa_only_scores[:, 2])))
+#
+# both_scores = np.array(both_scores)
+# print("\nAA With DCAA:")
+# print("Mean AA Coefficient: {0}".format(np.mean(both_scores[:, 0])))
+# print("Mean DCAA Coefficient: {0}".format(np.mean(both_scores[:, 1])))
+# print("Mean AUROC: {0}".format(np.mean(both_scores[:, 2])))
+# print("Mean Avg Precision: {0}".format(np.mean(both_scores[:, 3])))
+
+# aa_only_scores = np.array(aa_only_scores)
+# print("AA Only:")
+# print("Mean AA Coefficient: {0}".format(np.mean(aa_only_scores[:, 0])))
+# print("Mean AA Coefficient ** 2: {0}".format(np.mean(aa_only_scores[:, 1])))
+# print("Mean AUROC: {0}".format(np.mean(aa_only_scores[:, 2])))
+# print("Mean Avg Precision: {0}".format(np.mean(aa_only_scores[:, 3])))
+#
+# both_scores = np.array(both_scores)
+# print("\nAA With DCAA:")
+# print("Mean AA Coefficient: {0}".format(np.mean(both_scores[:, 0])))
+# print("Mean AA Coefficient ** 2: {0}".format(np.mean(both_scores[:, 2])))
+# print("Mean DCAA Coefficient: {0}".format(np.mean(both_scores[:, 1])))
+# print("Mean DCAA Coefficient ** 2: {0}".format(np.mean(both_scores[:, 3])))
+# print("Mean AA * DCAA Coefficient: {0}".format(np.mean(both_scores[:, 4])))
+# print("Mean AUROC: {0}".format(np.mean(both_scores[:, 5])))
+# print("Mean Avg Precision: {0}".format(np.mean(both_scores[:, 6])))
