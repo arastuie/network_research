107bf2c00f30c50c808069d3b9da64883307d8ef
Makan Arastuie <makan.arastuie@gmail.com>
2018-07-04 14:12:16 -0400

    Fixed fb local empirical analysis to separate before and after PYMK

--- main/Code/facebook_helpers.py
+++ main/Code/facebook_helpers.py
@@ -68,86 +68,110 @@ def extract_all_ego_centric_networks_in_fb(original_graph):
 
 
 def run_local_degree_empirical_analysis(ego_net_file):
-    # return if the egonet is on the analyzed list
-    if os.path.isfile(empirical_pickle_path + 'analyzed_egonets/' + ego_net_file):
+    # This analysis is separated into befor and after PYMK. FB intorduced PYMK around March 2008, which means
+    # links created between snapshots 0 to 5 are for before PYMK and the ones made between 5 to 9 are after.
+    # Also since not all ego nets have 10 snapshots, the index for before and after must be first calculated.
+
+    # return if the egonet is on the analyzed list in after pymk, it must be in before as well
+    if os.path.isfile(empirical_pickle_path + 'after-pymk/analyzed_egonets/' + ego_net_file):
         return
 
     with open(egonet_files_path + ego_net_file, 'rb') as f:
         ego, ego_snaps = pickle.load(f)
 
-    snapshots_local_formed = []
-    snapshots_global_formed = []
-    snapshots_local_not_formed = []
-    snapshots_global_not_formed = []
+    num_snaps = len(ego_snaps)
 
-    for i in range(len(ego_snaps) - 1):
-        # Get a list of v nodes which did or did not form an edge with the ego in the next snapshot
-        current_snap_z_nodes = set(ego_snaps[i].neighbors(ego))
-        current_snap_v_nodes = set(ego_snaps[i].nodes()) - current_snap_z_nodes
-        current_snap_v_nodes.remove(ego)
+    for pymk_type in ['before-pymk', 'after-pymk']:
+        if pymk_type == 'before-pymk':
+            # at least need 6 snapshots to have 1 snapshot jump for before PYMK
+            before_pymk_ending_snap = num_snaps - 5
+            if before_pymk_ending_snap < 1:
+                continue
 
-        next_snap_z_nodes = set(ego_snaps[i + 1].neighbors(ego))
+            start = 0
+            end = before_pymk_ending_snap
 
-        formed_v_nodes = next_snap_z_nodes.intersection(current_snap_v_nodes)
-        not_formed_v_nodes = current_snap_v_nodes - formed_v_nodes
+        else:
+            after_pymk_starting_snap = num_snaps - 5
+            if after_pymk_starting_snap < 0:
+                after_pymk_starting_snap = 0
 
-        if len(formed_v_nodes) == 0 or len(not_formed_v_nodes) == 0:
-            continue
+            start = after_pymk_starting_snap
+            end = num_snaps - 1
 
-        len_first_hop = len(current_snap_z_nodes)
-        tot_num_nodes = ego_snaps[i].number_of_nodes()
+        snapshots_local_formed = []
+        snapshots_global_formed = []
+        snapshots_local_not_formed = []
+        snapshots_global_not_formed = []
 
-        z_local_formed = []
-        z_global_formed = []
-        z_local_not_formed = []
-        z_global_not_formed = []
+        for i in range(start, end):
+            # Get a list of v nodes which did or did not form an edge with the ego in the next snapshot
+            current_snap_z_nodes = set(ego_snaps[i].neighbors(ego))
+            current_snap_v_nodes = set(ego_snaps[i].nodes()) - current_snap_z_nodes
+            current_snap_v_nodes.remove(ego)
 
-        # Getting degrees of the z nodes to speed up the process
-        z_local_degree = {}
-        z_global_degree = {}
-        for z in current_snap_z_nodes:
-            z_local_degree[z] = len(list(nx.common_neighbors(ego_snaps[i], z, ego)))
-            z_global_degree[z] = nx.degree(ego_snaps[i], z)
+            next_snap_z_nodes = set(ego_snaps[i + 1].neighbors(ego))
 
-        for v in formed_v_nodes:
-            common_neighbors = nx.common_neighbors(ego_snaps[i], v, ego)
-            temp_local = []
-            temp_global = []
+            formed_v_nodes = next_snap_z_nodes.intersection(current_snap_v_nodes)
+            not_formed_v_nodes = current_snap_v_nodes - formed_v_nodes
 
-            for cn in common_neighbors:
-                temp_local.append(z_local_degree[cn])
-                temp_global.append(z_global_degree[cn])
+            if len(formed_v_nodes) == 0 or len(not_formed_v_nodes) == 0:
+                continue
 
-            z_local_formed.append(np.mean(temp_local))
-            z_global_formed.append(np.mean(temp_global))
+            len_first_hop = len(current_snap_z_nodes)
+            tot_num_nodes = ego_snaps[i].number_of_nodes()
 
-        for v in not_formed_v_nodes:
-            common_neighbors = list(nx.common_neighbors(ego_snaps[i], v, ego))
-            temp_local = []
-            temp_global = []
+            z_local_formed = []
+            z_global_formed = []
+            z_local_not_formed = []
+            z_global_not_formed = []
 
-            for cn in common_neighbors:
-                temp_local.append(z_local_degree[cn])
-                temp_global.append(z_global_degree[cn])
+            # Getting degrees of the z nodes to speed up the process
+            z_local_degree = {}
+            z_global_degree = {}
+            for z in current_snap_z_nodes:
+                z_local_degree[z] = len(list(nx.common_neighbors(ego_snaps[i], z, ego)))
+                z_global_degree[z] = nx.degree(ego_snaps[i], z)
 
-            z_local_not_formed.append(np.mean(temp_local))
-            z_global_not_formed.append(np.mean(temp_global))
+            for v in formed_v_nodes:
+                common_neighbors = nx.common_neighbors(ego_snaps[i], v, ego)
+                temp_local = []
+                temp_global = []
 
-        snapshots_local_formed.append(np.mean(z_local_formed) / len_first_hop)
-        snapshots_global_formed.append(np.mean(z_global_formed) / tot_num_nodes)
-        snapshots_local_not_formed.append(np.mean(z_local_not_formed) / len_first_hop)
-        snapshots_global_not_formed.append(np.mean(z_global_not_formed) / tot_num_nodes)
+                for cn in common_neighbors:
+                    temp_local.append(z_local_degree[cn])
+                    temp_global.append(z_global_degree[cn])
 
-    if len(snapshots_local_formed) > 0:
-        with open(empirical_pickle_path + 'results/' + ego_net_file, 'wb') as f:
-            pickle.dump([np.mean(snapshots_local_formed),
-                         np.mean(snapshots_global_formed),
-                         np.mean(snapshots_local_not_formed),
-                         np.mean(snapshots_global_not_formed)], f, protocol=-1)
+                z_local_formed.append(np.mean(temp_local))
+                z_global_formed.append(np.mean(temp_global))
 
-    # save an empty file in analyzed_egonets to know which ones were analyzed
-    with open(empirical_pickle_path + 'analyzed_egonets/' + ego_net_file, 'wb') as f:
-        pickle.dump(0, f, protocol=-1)
+            for v in not_formed_v_nodes:
+                common_neighbors = list(nx.common_neighbors(ego_snaps[i], v, ego))
+                temp_local = []
+                temp_global = []
+
+                for cn in common_neighbors:
+                    temp_local.append(z_local_degree[cn])
+                    temp_global.append(z_global_degree[cn])
+
+                z_local_not_formed.append(np.mean(temp_local))
+                z_global_not_formed.append(np.mean(temp_global))
+
+            snapshots_local_formed.append(np.mean(z_local_formed) / len_first_hop)
+            snapshots_global_formed.append(np.mean(z_global_formed) / tot_num_nodes)
+            snapshots_local_not_formed.append(np.mean(z_local_not_formed) / len_first_hop)
+            snapshots_global_not_formed.append(np.mean(z_global_not_formed) / tot_num_nodes)
+
+        if len(snapshots_local_formed) > 0:
+            with open(empirical_pickle_path + pymk_type + '/results/' + ego_net_file, 'wb') as f:
+                pickle.dump([np.mean(snapshots_local_formed),
+                             np.mean(snapshots_global_formed),
+                             np.mean(snapshots_local_not_formed),
+                             np.mean(snapshots_global_not_formed)], f, protocol=-1)
+
+        # save an empty file in analyzed_egonets to know which ones were analyzed
+        with open(empirical_pickle_path + pymk_type + '/analyzed-egonets/' + ego_net_file, 'wb') as f:
+            pickle.dump(0, f, protocol=-1)
 
     print("Analyzed ego net {0}".format(ego_net_file))
     return
--- main/Code/undirected_analyze_egonets.py
+++ main/Code/undirected_analyze_egonets.py
@@ -5,11 +5,11 @@ from joblib import Parallel, delayed
 
 ### Facebook Empirical Local Degree Test
 all_egonets = set(os.listdir(fb.egonet_files_path))
-analyzed_egonets = set(os.listdir(fb.empirical_pickle_path + 'analyzed_egonets'))
+analyzed_egonets = set(os.listdir(fb.empirical_pickle_path + 'after-pymk/analyzed-egonets'))
 
 egonets_to_analyze = list(all_egonets - analyzed_egonets)
 np.random.shuffle(egonets_to_analyze)
 
 print("{} egonets left to analyze!".format(len(egonets_to_analyze)))
 
-Parallel(n_jobs=6)(delayed(fb.run_local_degree_empirical_analysis)(ego_net_file) for ego_net_file in egonets_to_analyze)
+Parallel(n_jobs=24)(delayed(fb.run_local_degree_empirical_analysis)(ego_net_file) for ego_net_file in egonets_to_analyze)
